a84a13def957f45f08969ec8f650086a18470ce9
Chingis <33646663+Chingis33@users.noreply.github.com>
2021-01-11 12:30:56 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ function reverseString(string $string): string
     $words = preg_split('#(\!|\.|\,|\?| )#mu', $string);
     foreach ($words as $word) {
         $revers = '';
-        $end = mb_strlen($word) -1;
+        $end = mb_strlen($word) - 1;
         for ($i = $end; $i >= 0; $i--) {
             $current = mb_substr($word, $i, 1);
             $revert = mb_substr($word, $end - $i, 1);
